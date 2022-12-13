785d6a524fe4d1d7f638db12e4bbf67e690c971a
Adisalem Bayu <Adisalem.bayu@yahoo.com>
2022-12-13 11:36:45 -0800

    Write a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "_putchar.h"
-
 /**
- * main - Prints _putchar
- * @void: Empty parameter list for main.
- *
- * Description: Prints _putchar w/out stdio
- *
+ * main - Prints "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
  * Return: 0 for success
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char _putchar[] = "_putchar";
+	int i = 0;
+
+	while (_putchar[i] != '\0')
+	{
+		_putchar(_putchar[i]);
+		i++;
+	}
 	_putchar('\n');
+
 	return (0);
 }
