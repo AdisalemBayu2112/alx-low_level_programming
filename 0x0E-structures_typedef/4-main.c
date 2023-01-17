96a85fa6929f15edc55f7dcc8fb4e85b1b04010b
Adisalem Bayu <Adisalem.bayu@yahoo.com>
2023-01-17 14:35:58 -0800

    Write a function that creates a new dog.

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,7 +12,13 @@ int main(void)
 	dog_t *my_dog;
 
 	my_dog = new_dog("Poppy", 3.5, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	if (my_dog == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	printf("My name is %s, I am %.2f :) - Woof!\n",
+			my_dog->name, my_dog->age);
 	return (0);
 }
 
