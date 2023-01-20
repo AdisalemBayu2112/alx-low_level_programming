ffa54cce56deb5399023602f803048724f3b34a4
Adisalem Bayu <Adisalem.bayu@yahoo.com>
2023-01-20 11:32:16 -0800

    Write a function that prints strings, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,14 @@
  * @n: The number of integers passed to the function.
  *  @...: A variable number of numbers to be printed.
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int index;
 
 	va_start(nums, n);
-	
+
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
